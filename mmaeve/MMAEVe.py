5b46b182c23a5e78b445eed7980ff13ac81eb345
AppleIntusion <lindsaysamuel19@gmail.com>
2024-02-08 11:20:41 -0500

    Quaternion exceptions

--- mmaeve/MMAEVe.py
+++ mmaeve/MMAEVe.py
@@ -196,23 +196,48 @@ def get_rot_quat(mobile, reference):
     Returns:   N x 3 np.array of Floats. Where each of the original 
                points has been aligned to a vector.
     '''
+    # Check if vector norms are the same or inverse
+    mobile_norm = mobile / \
+                  np.linalg.norm(mobile, axis = 1)[:, np.newaxis] 
+    ref_norm    = reference / \
+                  np.linalg.norm(reference, axis = 1)[:, np.newaxis]
+    dot = np.sum(mobile_norm * ref_norm, axis = 1)
+    same_vec = dot >  0.9999
+    inv_vec  = dot < -0.9999
+
     # Get the rotation axis and the angle to rotate about to align
     # "mobile" with "reference".
+    # Numpy may throw a div by zero warning in the case of inverse
+    # or aligned vectors. It will be handled.
     rot_axis = np.cross(mobile, reference)
+    rot_axis = rot_axis / \
+               np.reshape(np.linalg.norm(rot_axis, axis = 1), 
+                          [len(rot_axis), 1]) # Normalize
     dot = np.sum(mobile * reference, axis = 1)
     angle = np.arccos(dot / \
               (np.linalg.norm(mobile, axis = 1)     * \
                np.linalg.norm(reference, axis = 1)
               )
             )
-    rot_axis = rot_axis / \
-               np.reshape(np.linalg.norm(rot_axis, axis = 1), 
-                          [len(rot_axis), 1]) # Normalize
+
+    # Handle the case of inverse vectors. A 180 degree rotation about 
+    # the y-axis is performed.
+    rot_axis[inv_vec, 0] = 0.
+    rot_axis[inv_vec, 1] = 1.
+    rot_axis[inv_vec, 2] = 0.
+    angle[inv_vec] = np.pi
+
     # Create the rotation and inverse rotation quaternions.
     cos_half = np.cos(angle / 2.)
     sin_half = np.sin(angle / 2.)
     q0, q1, q2, q3 = cos_half,                  rot_axis[:, 0] * sin_half, \
                      rot_axis[:, 1] * sin_half, rot_axis[:, 2] * sin_half
+    # Handle the case of aligned vectors. A zero rotation is performed.
+    q0[same_vec] = 1.
+    q1[same_vec] = 0.
+    q2[same_vec] = 0.
+    q3[same_vec] = 0.
+
     rot_quat = np.array([q0, q1, q2, q3])
     rot_quat = rot_quat.T
     inv_quat = np.array([q0, -q1, -q2, -q3])
